2aa3f8d03a3621bd96b6efb9a1788b819a53f3cd
b6land <20431288+b6land@users.noreply.github.com>
2023-01-02 11:22:04 +0800

    feat: DLL with a simple add method

---
+++ dllmain.cpp
@@ -0,0 +1,28 @@
+﻿// dllmain.cpp : 定義 DLL 應用程式的進入點。
+#include "pch.h"
+
+extern "C" {
+    __declspec(dllexport) int __cdecl Add(int a, int b);
+}
+
+extern int __cdecl Add(int num1, int num2) {
+    return num1 + num2;
+    
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
